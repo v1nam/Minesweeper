6a051239d115513fd0d8f6f977e897c9586a1b13
v1nam <vinamraj01@gmail.com>
2021-08-20 21:04:11 +0530

    split code into multiple files

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,18 @@
-#include <sstream>
 #include <vector>
-#include <algorithm>
 
 #include "raylib.h"
-
-// from https://nordtheme.com
-const Color bgCol  = Color{46, 52, 64, 255};
-const Color red    = Color{191, 97, 106, 255};
-const Color hlt    = Color{76, 86, 106, 255};
-const Color teal   = Color{143, 188, 187, 255};
-const Color aqua   = Color{136, 192, 208, 255};
-const Color blue   = Color{94, 129, 172, 255};
-const Color gray   = Color{67, 76, 94, 255};
-const Color lblue  = Color{129, 161, 193, 255};
-const Color green  = Color{163, 190, 140, 255};
-const Color yellow = Color{235, 203, 139, 255};
-const Color orange = Color{208, 135, 112, 255};
-
-
-struct Cell
-{
-    bool revealed = false;
-    int value = 0;
-    Color color = gray;
-};
-
-std::vector<Vector2> adjacents(std::vector<std::vector<Cell>> &grid, int x, int y, int rows, int columns)
-{
-    std::vector<Vector2> adjc;
-    for (int i{-1}; i <= 1; i++)
-    {
-        for (int j{-1}; j <= 1; j++) {
-            int ax = x + i;
-            int ay = y + j;
-            if (ax >= 0 && ay >= 0 && ax < columns && ay < rows && (ax != x || ay != y))
-                adjc.push_back(Vector2{(float)ax, (float)ay});
-        }
-    }
-    return adjc;
-}
-
-int countMines(std::vector<std::vector<Cell>> &grid, std::vector<Vector2>& adjc)
-{
-    int mc = 0;
-    for (Vector2& adPos : adjc)
-    {
-        if (grid[(int)adPos.x][(int)adPos.y].value == -1)
-            mc++;
-    }
-    return mc;
-}
-
-void reveal(std::vector<std::vector<Cell>> &grid, int x, int y, int rows, int columns)
-{
-    if (!grid[x][y].revealed && grid[x][y].value != -1)
-    {
-        std::vector<Vector2> adjc = adjacents(grid, x, y, rows, columns);
-        grid[x][y].value = countMines(grid, adjc);
-        grid[x][y].revealed = true;
-
-        if (grid[x][y].value == 0)
-        {
-            for (Vector2& adPos : adjc)
-                reveal(grid, adPos.x, adPos.y, rows, columns);
-        }
-    }
-}
-
-void initGame(std::vector<std::vector<Cell>> &grid, int x, int y, int mineCount, int rows, int columns)
-{
-    std::vector<int> minePosX = {x};
-    std::vector<int> minePosY = {y};
-    for (int i{0}; i < mineCount;)
-    {
-        int mineX = GetRandomValue(0, columns - 1);
-        int mineY = GetRandomValue(0, rows - 1);
-
-        if (std::find(minePosX.begin(), minePosX.end(), mineX) == minePosX.end() || std::find(minePosY.begin(), minePosY.end(), mineY) == minePosY.end())
-        {
-            i++;
-            grid[mineX][mineY].value = -1; // -1 means this cell is a mine
-            minePosX.push_back(mineX);
-            minePosY.push_back(mineY);
-        }
-    }
-    reveal(grid, x, y, rows, columns);
-}
+#include "minesweeper.hpp"
+#include "colors.hpp"
 
 int main()
 {
     const int screenWidth = 436;
     const int screenHeight = 436;
 
-    const int rows = 8;
-    const int columns = 8;
-    const int pad = 4;
+    Minesweeper mnsp = Minesweeper();
 
-    const int cellSize = (screenWidth - pad * (columns + 1)) / columns;
-    const int mineCount = 10;
+    const int pad = 4;
+    const int cellSize = (screenWidth - pad * (mnsp.columns + 1)) / mnsp.columns;
 
     const std::vector<Color> colArr = {Color{96, 106, 126, 255}, green, yellow, orange, red, teal, aqua, lblue, blue};
     char const* numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8"}; 
@@ -107,10 +22,6 @@ int main()
     SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(screenWidth, screenHeight, "Minesweeper");
 
-    std::vector<std::vector<Cell>> grid(columns);
-    for (int j{0}; j < columns; j++)
-        grid[j] = std::vector<Cell>(rows);
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -119,13 +30,13 @@ int main()
         int mouseHoverX = GetMouseX();
         int mouseHoverY = GetMouseY();
 
-        for (int x{0}; x < columns; x++)
+        for (int x{0}; x < mnsp.columns; x++)
         {
-            for (int y{0}; y < rows; y++)
+            for (int y{0}; y < mnsp.rows; y++)
             {
                 Rectangle cellPos = Rectangle{(float)pad + x * (cellSize + pad), (float)pad + y * (cellSize + pad),
                                            (float)cellSize, (float)cellSize};
-                Cell cell = grid[x][y];
+                Cell cell = mnsp.grid[x][y];
                 Color cellCol = cell.color;
 
                 // checking if cursor is hovering on a cell
@@ -137,9 +48,9 @@ int main()
                     {
                         if (!started)
                         {
-                            initGame(grid, x, y, mineCount, rows, columns);
+                            mnsp.initGame(x, y);
                             started = true;
-                        } else reveal(grid, x, y, rows, columns);
+                        } else mnsp.reveal(x, y);
                     }
                 }
                 if (cell.revealed && cell.value >= 0)
