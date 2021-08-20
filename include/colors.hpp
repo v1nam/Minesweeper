d8ab540a3f6a3db915c9716c13f74cfc2b3710be
v1nam <vinamraj01@gmail.com>
2021-08-20 21:04:30 +0530

    put all color definitions in header file

---
+++ include/colors.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "raylib.h"
+
+// from https://nordtheme.com
+const Color bgCol  = Color{46, 52, 64, 255};
+const Color red    = Color{191, 97, 106, 255};
+const Color hlt    = Color{76, 86, 106, 255};
+const Color teal   = Color{143, 188, 187, 255};
+const Color aqua   = Color{136, 192, 208, 255};
+const Color blue   = Color{94, 129, 172, 255};
+const Color gray   = Color{67, 76, 94, 255};
+const Color lblue  = Color{129, 161, 193, 255};
+const Color green  = Color{163, 190, 140, 255};
+const Color yellow = Color{235, 203, 139, 255};
+const Color orange = Color{208, 135, 112, 255};
