5e77935fb0828d8f77963acf264c7ddb9ce4fbaf
v1nam <vinamraj01@gmail.com>
2021-08-20 21:05:04 +0530

    move all functions from main file

---
+++ src/minesweeper.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+#include <algorithm>
+
+#include "raylib.h"
+#include "minesweeper.hpp"
+
+Minesweeper::Minesweeper(int rows, int columns, int mineCount)
+{
+    this->rows = rows;
+    this->columns = columns;
+    this->mineCount = mineCount;
+
+    for (int _j{0}; _j < this->columns; _j++)
+        grid.push_back(std::vector<Cell>(this->rows));
+}
+
+std::vector<Vector2> Minesweeper::adjacents(int x, int y)
+{
+    std::vector<Vector2> adjc;
+    for (int i{-1}; i <= 1; i++)
+    {
+        for (int j{-1}; j <= 1; j++) {
+            int ax = x + i;
+            int ay = y + j;
+            if (ax >= 0 && ay >= 0 && ax < columns && ay < rows && (ax != x || ay != y))
+                adjc.push_back(Vector2{(float)ax, (float)ay});
+        }
+    }
+    return adjc;
+}
+
+int Minesweeper::countMines(std::vector<Vector2>& adjc)
+{
+    int mc = 0;
+    for (Vector2& adPos : adjc)
+    {
+        if (grid[(int)adPos.x][(int)adPos.y].value == -1)
+            mc++;
+    }
+    return mc;
+}
+
+void Minesweeper::reveal(int x, int y)
+{
+    if (!grid[x][y].revealed && grid[x][y].value != -1)
+    {
+        std::vector<Vector2> adjc = adjacents(x, y);
+        grid[x][y].value = countMines(adjc);
+        grid[x][y].revealed = true;
+
+        if (grid[x][y].value == 0)
+        {
+            for (Vector2& adPos : adjc)
+                this->reveal(adPos.x, adPos.y);
+        }
+    }
+}
+
+void Minesweeper::initGame(int x, int y)
+{
+    std::vector<int> minePosX = {x};
+    std::vector<int> minePosY = {y};
+    for (int i{0}; i < mineCount;)
+    {
+        int mineX = GetRandomValue(0, columns - 1);
+        int mineY = GetRandomValue(0, rows - 1);
+
+        if (std::find(minePosX.begin(), minePosX.end(), mineX) == minePosX.end() || std::find(minePosY.begin(), minePosY.end(), mineY) == minePosY.end())
+        {
+            i++;
+            grid[mineX][mineY].value = -1; // -1 means this cell is a mine
+            minePosX.push_back(mineX);
+            minePosY.push_back(mineY);
+        }
+    }
+    this->reveal(x, y);
+}
