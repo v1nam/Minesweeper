604d9893b82347fce5078bc0f9e2ad7e0bf20059
v1nam <vinamraj01@gmail.com>
2021-08-21 20:30:18 +0530

    add Start Menu and format code

--- include/display.hpp
+++ include/display.hpp
@@ -1,29 +1,33 @@
 #pragma once
 
-#include "raylib.h"
-#include "minesweeper.hpp"
 #include "colors.hpp"
+#include "minesweeper.hpp"
+#include "raylib.h"
 
-enum class State
-{
+enum class State {
     Paused,
     Playing,
-    GameOver
+    GameOver,
+    StartMenu
 };
 
-class Display
-{
-    public:
-        int pad;
-        int cellSize;
+class Display {
+public:
+    int screenWidth;
+    int screenHeight;
+    int pad;
+    int cellSize;
 
-        State state = State::Playing;
+    Rectangle menuBtn1;
+    Rectangle menuBtn2;
 
-        const std::vector<Color> colArr = {Color{96, 106, 126, 255}, green, yellow, orange, red, teal, aqua, lblue, blue};
-        char const* numbers[8] = {"1", "2", "3", "4", "5", "6", "7", "8"}; 
+    State state = State::Playing;
 
-        Minesweeper mnsp;
+    const std::vector<Color> colArr = { Color { 96, 106, 126, 255 }, green, yellow, orange, red, teal, aqua, lblue, blue };
+    char const* numbers[8] = { "1", "2", "3", "4", "5", "6", "7", "8" };
 
-        Display(int cellSize, int pad, Minesweeper mnsp);
-        void draw();
+    Minesweeper mnsp;
+
+    Display(int cellSize, int pad, Minesweeper mnsp, int screenWidth, int screenHeight);
+    void draw();
 };
