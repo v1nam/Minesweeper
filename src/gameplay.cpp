32f2217545139907b6ce4d70fcf1b0a5d0e0be48
v1nam <vinamraj01@gmail.com>
2021-08-26 22:25:59 +0530

    move game logic out of for loop for optimization

--- src/gameplay.cpp
+++ src/gameplay.cpp
@@ -27,6 +27,36 @@ void Display::drawGame()
         },
         pbtnRef);
 
+    int cellHoverX = ((int)mouseHoverX - pad) / (cellSize + pad);
+    int cellHoverY = ((int)mouseHoverY - pad) / (cellSize + pad);
+    if (cellHoverX < mnsp.columns && cellHoverY < mnsp.rows) {
+        Cell& hoverCell = mnsp.grid[cellHoverX][cellHoverY];
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if (!mnsp.started)
+                mnsp.initGame(cellHoverX, cellHoverY);
+            else if (hoverCell.value == -1) {
+                mnsp.won = false;
+                hoverCell.revealed = true;
+                if (hoverCell.flagged) {
+                    hoverCell.flagged = false;
+                    mnsp.flagCount--;
+                }
+                state = State::GameOver;
+            } else
+                mnsp.reveal(cellHoverX, cellHoverY);
+        } else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            if (!hoverCell.revealed && mnsp.started) {
+                if (hoverCell.flagged) {
+                    hoverCell.flagged = false;
+                    mnsp.flagCount--;
+                } else if (mnsp.flagCount < mnsp.mineCount) {
+                    hoverCell.flagged = true;
+                    mnsp.flagCount++;
+                }
+            }
+        }
+    }
     for (int x { 0 }; x < mnsp.columns; x++) {
         for (int y { 0 }; y < mnsp.rows; y++) {
             Rectangle cellPos = Rectangle { (float)pad + x * (cellSize + pad), (float)pad + y * (cellSize + pad),
@@ -34,35 +64,9 @@ void Display::drawGame()
             Cell& cell = mnsp.grid[x][y];
             Color cellCol = cell.color;
 
-            // checking if cursor is hovering on a cell
-            if (CheckCollisionPointRec(Vector2 { mouseHoverX, mouseHoverY }, cellPos)) {
+            if (cellHoverX == x && cellHoverY == y)
                 cellCol = hlt;
-                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                    if (!mnsp.started)
-                        mnsp.initGame(x, y);
-                    else if (cell.value == -1) {
-                        mnsp.won = false;
-                        cell.revealed = true;
-                        if (cell.flagged) {
-                            cell.flagged = false;
-                            mnsp.flagCount--;
-                        }
-                        state = State::GameOver;
-                        //DrawCircle(cellPos.x + cellSize / 2, cellPos.y + cellSize / 2, cellSize / 4, red);
-                    } else
-                        mnsp.reveal(x, y);
-                } else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-                    if (!cell.revealed) {
-                        if (cell.flagged) {
-                            cell.flagged = false;
-                            mnsp.flagCount--;
-                        } else if (mnsp.flagCount < mnsp.mineCount) {
-                            cell.flagged = true;
-                            mnsp.flagCount++;
-                        }
-                    }
-                }
-            }
+
             if (cell.revealed && cell.value >= 0)
                 cellCol = colArr[cell.value];
 
