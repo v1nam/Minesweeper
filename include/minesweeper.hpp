6685836851f0d46e82e07a03e15969e9a91d784c
v1nam <vinamraj01@gmail.com>
2021-08-27 16:52:39 +0530

    add CellVec class for storing cell position and add a vector to store cell pointers which need to be drawn

--- include/minesweeper.hpp
+++ include/minesweeper.hpp
@@ -7,11 +7,28 @@
 #include "colors.hpp"
 #include "raylib.h"
 
+class CellVec {
+public:
+    int x;
+    int y;
+
+    bool operator==(const CellVec other) const { return x == other.x && y == other.y; };
+};
+
+class HashFunction {
+public:
+    size_t operator()(const CellVec& cv) const
+    {
+        return (std::hash<int>()(cv.x)) ^ (std::hash<int>()(cv.y));
+    }
+};
+
 struct Cell {
     bool revealed = false;
     int value = 0;
     Color color = gray;
     bool flagged = false;
+    CellVec pos;
 };
 
 class Minesweeper {
@@ -28,19 +45,17 @@ public:
     bool started = false;
     bool won = false;
 
-    std::unordered_set<int*> flagCoords;
+    std::unordered_set<CellVec, HashFunction> flagCoords;
 
     std::vector<std::vector<Cell>> grid;
+    std::vector<Cell*> needsUpdate;
+
     char const* endText = "Lost";
 
     Minesweeper(int rows = 8, int columns = 8, int mineCount = 10);
     std::vector<Vector2> adjacents(int x, int y);
     int countMines(std::vector<Vector2>& adjc);
-    void reveal(int x, int y, RenderTexture2D& gridTex,
-        Rectangle& cellPos,
-        int& cellSize,
-        const std::vector<const char*>& numbers,
-        const std::vector<Color>& colArr);
+    void reveal(int x, int y);
     void initGame(int x, int y);
     void clear();
     std::string flagCountDisplay();
