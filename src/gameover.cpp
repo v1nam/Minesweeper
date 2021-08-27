6a070857f2d300ed5c2ab1239a2fca9ac32f50f4
v1nam <vinamraj01@gmail.com>
2021-08-27 17:04:23 +0530

    draw extra things on game play texture and remove drawing the game over texture

--- src/gameover.cpp
+++ src/gameover.cpp
@@ -8,19 +8,12 @@
 void Display::drawGameOver()
 {
     if (!textureMade) {
-        BeginTextureMode(gameOverTexture);
+        BeginTextureMode(gamePlayTexture);
         {
-            ClearBackground(bgCol);
             for (int x { 0 }; x < mnsp.columns; x++) {
                 for (int y { 0 }; y < mnsp.rows; y++) {
-                    Rectangle cellPos = Rectangle { (float)pad + x * (cellSize + pad), (float)pad + y * (cellSize + pad),
-                        (float)cellSize, (float)cellSize };
+                    Rectangle cellPos = indexToPos(x, y);
                     Cell& cell = mnsp.grid[x][y];
-                    Color cellCol = cell.color;
-                    if (cell.revealed && cell.value >= 0)
-                        cellCol = colArr[cell.value];
-
-                    DrawRectangleRounded(cellPos, 0.1, 0, cellCol);
 
                     if (cell.value == -1) {
                         DrawRectangleRounded(cellPos, 0.1, 0, Fade(red, 0.4));
@@ -40,12 +33,6 @@ void Display::drawGameOver()
                             Vector2 { cellPos.x + (scale * flag.width) / 3.f, cellPos.y + (scale * flag.width) / 3.f },
                             0.0, scale, WHITE);
                     }
-                    if (cell.value > 0) {
-                        const char* text = numbers[cell.value - 1];
-                        float size = cellSize / 2.0;
-                        DrawText(text, (cellPos.x + cellSize / 2) - (MeasureText(text, size) / 2),
-                            cellPos.y + cellSize / 4, size, bgCol);
-                    }
                 }
             }
         }
@@ -53,7 +40,7 @@ void Display::drawGameOver()
         textureMade = true;
     } else {
         ClearBackground(bgCol);
-        DrawTextureRec(gameOverTexture.texture, Rectangle { 0, 0, 548, -548 }, Vector2 { 0, 0 }, WHITE);
+        DrawTextureRec(gamePlayTexture.texture, Rectangle { 0, 0, 548, -548 }, Vector2 { 0, 0 }, WHITE);
         DrawText("You", (screenWidth - 150) + (75 - MeasureText("You", 25) / 2.), screenHeight / 2 + 35, 25, lblue);
         DrawText(mnsp.endText, (screenWidth - 150) + (75 - MeasureText(mnsp.endText, 30) / 2.), screenHeight / 2. + 70,
             30, mnsp.won ? green : red);
